f06faf485d2e9de537da2e836af20634f1063bc6
clement-lnr <clementlanneretonne@yahoo.fr>
2024-11-20 23:01:30 +0100

    feat(frontend): basic toDo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import styles from './index.module.scss'
 
 function App() {
+  const [toDoList, setToDoList] = useState([{}]);
+  const [ newTask, setNewTask ] = useState('')
+
+  const addTask = () => {
+    setToDoList([...toDoList, { id: toDoList.length + 1, text: newTask, checked: false }])
+  }
 
   return (
     <>
       <h1 className={`${styles.title}`}>ToDo</h1>
+      <input type="text" onChange={(e) => setNewTask(e.target.value)}/>
+      <button onClick={() => addTask()}>Add</button>
+      
+      {
+        toDoList.map((item) => (
+          <a>{item.text}</a>
+        ))
+      }
     </>
   )
 }
