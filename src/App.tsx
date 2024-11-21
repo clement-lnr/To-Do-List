0ca86781ece4a134d43a8ca5fab1ce88596622d1
clement-lnr <clementlanneretonne@yahoo.fr>
2024-11-21 22:27:50 +0100

    style: app basic style

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,24 @@ function App() {
   }
 
   return (
-    <>
+    <div>
       <h1 className={`${styles.title}`}>ToDo</h1>
-      <input type="text" onChange={(e) => setNewTask(e.target.value)}/>
-      <button onClick={() => addTask()}>Add</button>
+
+      <div className={`${styles.todohandler}`}>
+        <input type="text" placeholder='Nom de la tache...' onChange={(e) => setNewTask(e.target.value)} className={`${styles.addinput}`}/>
+        <button onClick={() => addTask()} className={`${styles.addbtn}`}>Ajouter la tache</button>
+      </div>
       
-      {
-        toDoList.map((item) => (
-          <a>{item.text}</a>
-        ))
-      }
-    </>
+      <div className={`${styles.todolist}`}>
+        {
+          toDoList.map((item) => (
+            <div className={`${styles.task}`} key={item.id}>
+              <a>{item.text}</a>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
